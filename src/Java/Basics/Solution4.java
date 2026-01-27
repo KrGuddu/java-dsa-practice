d7a87a34387b64ff4dff5d325e96b2bfea0cdeb5
KrGuddu <krguddu684@gmail.com>
2026-01-27 13:16:24 +0530

    update

--- src/Java/Basics/Solution4.java
+++ src/Java/Basics/Solution4.java
@@ -1,42 +1,37 @@
-/* Task: Given an integer, , perform the following conditional actions:
-
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Complete the stub code provided in your editor to print whether or not  is weird.
+/* In each line of output there should be two columns:
+The first column contains the String and is left justified using exactly 15 characters.
+The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
 */
 
-/* How it works:
-1. if (n % 2 != 0) → Checks if n is odd. Odd numbers are Weird.
-2. else → Means n is even, so we check ranges:
-    2 to 5 → Not Weird
-    6 to 20 → Weird
-    Greater than 20 → Not Weird
+/* Explanation:
+    %-15s → Left-justifies the string in a field of 15 characters.
+    %03d → Prints the integer in 3 digits, padding with leading zeros if needed.
+    %n → New line.
 */
 
 import java.util.Scanner;
 
 public class Solution4 {
-    // private static final Scanner scanner = new Scanner(System.in);      //Ye ek single scanner object banata hai jo poori class me reuse ho sakta hai — especially Hackerrank jaise platforms pe ye default template hota hai. So isme aur Scanner scan = new Scanner(System.in); dono me se koe ek hi likhe.
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        scan.close();
+        Scanner sc = new Scanner(System.in);
 
-        if (n % 2 != 0) {
-            // n is odd
-            System.out.println("Weird");
-        } else {
-            // n is even
-            if (n >= 2 && n <= 5) {
-                System.out.println("Not Weird");
-            } else if (n >= 6 && n <= 20) {
-                System.out.println("Weird");
-            } else if (n > 20) {
-                System.out.println("Not Weird");
-            }
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s = sc.next();
+            int x = sc.nextInt();
+            System.out.printf("%-15s%03d%n", s, x);
         }
+        System.out.println("================================");
+
+        sc.close();
     }
 }
+
+
+/* Output: 
+================================
+java           100
+cpp            065
+python         050
+================================
+ */
