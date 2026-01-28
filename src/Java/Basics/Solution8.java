895de2086b7bc177ad25b5a5dc839f614beccfd1
KrGuddu <krguddu684@gmail.com>
2026-01-28 22:51:27 +0530

    update

--- src/Java/Basics/Solution8.java
+++ src/Java/Basics/Solution8.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Solution8 {
-    
+    public static void main(String[] argh) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            
+        }
+        sc.close();
+    }
 }
