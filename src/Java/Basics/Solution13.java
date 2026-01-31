8a85253b7ababf804f4a74b4c99f27700d1aaeb8
KrGuddu <krguddu684@gmail.com>
2026-01-31 19:21:13 +0530

    Solution13

---
+++ src/Java/Basics/Solution13.java
@@ -0,0 +1,29 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // US Locale
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+
+        // India Locale (custom)
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+
+        // China Locale
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+
+        // France Locale
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
