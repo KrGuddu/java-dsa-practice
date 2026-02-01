4c528f3dfc0b51f17d3d13bdf8fea368e9a8fb8d
KrGuddu <krguddu684@gmail.com>
2026-02-01 17:56:02 +0530

    Solution1 problem Statements

--- src/Java/String/Solution1.java
+++ src/Java/String/Solution1.java
@@ -1,3 +1,20 @@
+/* Problem Statement: You are given two strings A and B (only lowercase English letters).
+You need to:
+1. Print the sum of their lengths.
+2. Check if string A is lexicographically greater than string B.
+    If yes, print Yes
+    Otherwise, print No
+3. Capitalize the first letter of both strings and print them on one line separated by a space.
+
+Input:- 
+First line: String A
+Second line: String B
+
+Output:- 
+Line 1: Sum of lengths of A and B
+Line 2: Yes or No
+Line 3: Capitalized A and B separated by a space
+*/
 package Java.String;
 
 import java.util.Scanner;
@@ -8,14 +25,17 @@ public class Solution1 {
         String A = sc.next();
         String B = sc.next();
 
+        // 1. Sum of lengths
         System.out.println(A.length() + B.length());
 
+        // 2. Lexicographical comparison
         if (A.compareTo(B) > 0) {
             System.out.println("Yes");;
         } else {
             System.out.println("No");
         }
         
+        // 3. Capitalize first letter of both strings
         String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
         String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
         System.out.println(capA + " " + capB);
