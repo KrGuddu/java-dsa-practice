927cc2c594c0eae61da68b57565674471297e456
KrGuddu <krguddu684@gmail.com>
2026-02-01 18:27:37 +0530

    problem statement

--- src/Java/String/Solution2.java
+++ src/Java/String/Solution2.java
@@ -1,3 +1,23 @@
+/* Problem Statement:- You are given:
+• A string s
+• Two integers start and end
+So, Your task is to print the substring of s that starts at index start and ends at index end - 1 (end is not included).
+
+Input: 
+1. A string s
+2. Two space-separated integers: start and end
+
+Output: Print the substring from index start (inclusive) to end (exclusive).
+
+Example: 
+Input:
+Helloworld
+3 7
+
+Output: lowo
+*/
+
+
 package Java.String;
 import java.util.Scanner;
 
@@ -9,6 +29,7 @@ public class Solution2 {
         int start = sc.nextInt();
         int end = sc.nextInt();
 
+        // Print substring from start (inclusive) to end (exclusive)
         System.out.println(s.substring(start, end));
         sc.close();
     }
