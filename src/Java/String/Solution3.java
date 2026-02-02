a51ed7bdfc2bc9cf0ca4a0a9dcb14709751204b0
KrGuddu <krguddu684@gmail.com>
2026-02-02 16:13:01 +0530

    Problem statement

--- src/Java/String/Solution3.java
+++ src/Java/String/Solution3.java
@@ -1,5 +1,28 @@
-package Java.String;
+/* Problem Statement:- You are given:
+• A string s
+• An integer k
 
+Your task is to:
+1. Find all substrings of length k from the string s.
+2. Among those substrings:
+    • Find the lexicographically smallest substring.
+    • Find the lexicographically largest substring.
+3. Print the smallest substring on the first line and the largest substring on the second line.
+
+Example:- Input:
+welcometojava
+3
+
+Substrings of length 3: wel, elc, lco, com, ome, met, eto, toj, oja, jav, ava
+Lexicographically smallest: ava
+Lexicographically largest: wel
+
+Output:
+ava
+wel
+*/
+
+package Java.String;
 import java.util.Scanner;
 
 public class Solution3 {
