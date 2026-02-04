a4aeb302f487d42b68b7308098a1e7d7c3b5b706
KrGuddu <krguddu684@gmail.com>
2026-02-04 14:39:05 +0530

    Solution10

---
+++ src/Java/String/Solution10.java
@@ -0,0 +1,33 @@
+package Java.String;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution10 {
+    public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+        Pattern pattern = Pattern.compile("<([^>]+)>([^<>]+)</\\1>");
+        
+		while(testCases > 0){
+			String line = in.nextLine();
+			
+            Matcher matcher = pattern.matcher(line);
+            boolean found = false;
+            
+            while(matcher.find()){
+                System.out.println(matcher.group(2));
+                found = true;
+            }
+            
+            if(!found){
+                System.out.println("None");
+            }
+            
+			testCases--;
+		}
+        in.close();
+	}
+}
